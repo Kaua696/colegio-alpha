7dc83a07dc8aeccdd147dd275e45e536bd52bc9d
Kauã Henrique Gonçalves <kauamamga@gmail.com>
2023-05-31 16:52:24 -0300

    add atualizacao menu

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@
     if (window.pageYOffset > 0) {
       navbar.style.top = '0';
     } else {
-      navbar.style.top = '47px';
+      navbar.style.top = '43px';
     }
   });
 /* OFF MENU DROPDOWM */ 
